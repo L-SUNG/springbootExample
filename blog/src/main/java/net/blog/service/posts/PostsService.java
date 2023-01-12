976a2c7af557fb765baadb233b595a0e1fe3b718
lsung <cp.lsung@gmail.com>
2023-01-12 13:18:54 +0900

    포스트등록 API 작성

--- blog/src/main/java/net/blog/service/posts/PostsService.java
+++ blog/src/main/java/net/blog/service/posts/PostsService.java
@@ -1,2 +1,18 @@
-package net.blog.service.posts;public class PostsService {
+package net.blog.service.posts;
+
+import lombok.RequiredArgsConstructor;
+import net.blog.domain.posts.PostsRepository;
+import net.blog.web.dto.PostsSaveRequestDto;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@RequiredArgsConstructor
+@Service
+public class PostsService {
+    private final PostsRepository postsRepository;
+
+    @Transactional
+    public Long save(PostsSaveRequestDto requestDto) {
+        return postsRepository.save(requestDto.toEntity()).getId();
+    }
 }
--- blog/src/main/java/net/blog/web/PostsApiController.java
+++ blog/src/main/java/net/blog/web/PostsApiController.java
@@ -1,2 +1,20 @@
-package net.blog.web;public class PostsApiController {
+package net.blog.web;
+
+import lombok.RequiredArgsConstructor;
+import net.blog.service.posts.PostsService;
+import net.blog.web.dto.PostsSaveRequestDto;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RestController
+public class PostsApiController {
+
+    private final PostsService postsService;
+
+    @PostMapping("/api/v1/posts")
+    public Long save(@RequestBody PostsSaveRequestDto requestDto) {
+        return postsService.save(requestDto);
+    }
 }
