ab854e3b8a39a92e1a83f7ef3ba3ea3ef01a4d2a
lsung <cp.lsung@gmail.com>
2023-01-16 21:20:47 +0900

    게시글 삭제 처리 작성

--- blog/src/main/resources/static/js/app/index.js
+++ blog/src/main/resources/static/js/app/index.js
@@ -1,12 +1,17 @@
 var main = {
     init: function() {
         var _this = this;
+        // 글 등록 버튼 이벤트
         $('#btn-save').on('click', function() {
             _this.save();
         });
-
+        // 글 수정 버튼 이벤트
         $('#btn-update').on('click', function() {
             _this.update();
+        });
+        // 글 삭제 버튼 이벤트
+        $('#btn-delete').on('click', function() {
+            _this.delete();
         });
     },
     save: function() {
@@ -46,6 +51,22 @@ var main = {
             data: JSON.stringify(data)
         }).done(function() {
             alert('글이 수정되었습니다.');
+            window.location.href = '/';
+        }).fail(function(error) {
+            alert(JSON.stringify(error));
+        })
+    },
+    delete: function() {
+        // 게시글ID
+        var id = $('#id').val();
+
+        $.ajax({
+            type: 'DELETE',
+            url: '/api/v1/posts/' + id,
+            dataType: 'json',
+            contentType: 'application/json; charset=utf-8'
+        }).done(function() {
+            alert('글이 삭제되었습니다.');
             window.location.href = '/';
         }).fail(function(error) {
             alert(JSON.stringify(error));
