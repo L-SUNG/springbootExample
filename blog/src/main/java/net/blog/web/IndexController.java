634f39c44c6b5c53311e71ce09a39dd268868c99
lsung <cp.lsung@gmail.com>
2023-01-18 21:41:06 +0900

    LoginUser 어노테이션을 통해 user 취득부분의 코드개선

--- blog/src/main/java/net/blog/web/IndexController.java
+++ blog/src/main/java/net/blog/web/IndexController.java
@@ -1,6 +1,7 @@
 package net.blog.web;
 
 import lombok.RequiredArgsConstructor;
+import net.blog.config.auth.LoginUser;
 import net.blog.config.auth.dto.SessionUser;
 import net.blog.domain.posts.PostsRepository;
 import net.blog.service.posts.PostsService;
@@ -20,10 +21,11 @@ public class IndexController {
     private final HttpSession httpSession;
 
     @GetMapping("/")
-    public String index(Model model) {
+    public String index(Model model, @LoginUser SessionUser user) {
+
         model.addAttribute("posts", postsService.findAllDesc());
-        // 세션에서 user를 취득
-        SessionUser user = (SessionUser) httpSession.getAttribute("user");
+
+        // LoginUser 어노테이션을 통해 취득한 user를 판정
         if (user != null) {
             // 세션에서 취득한 user가 null이 아닐경우 userName에 유저명을 설정
             model.addAttribute("loginUserName", user.getName());
