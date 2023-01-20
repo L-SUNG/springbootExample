1170b7e429b8daca39468e5c734d90e51bcdbf9a
lsung <cp.lsung@gmail.com>
2023-01-20 14:24:41 +0900

    테스트코드 에러 해소 - WebMvcTest 어노테이션의 스캔범위 대상제외 코드 작성 - EnableJpaAuditing 어노테이션을 SpringBootApplication과 분리

--- blog/src/main/java/net/blog/Application.java
+++ blog/src/main/java/net/blog/Application.java
@@ -4,7 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-@EnableJpaAuditing // JPA Auditing 활성화
+// EnableJpaAuditing와 SpringBootApplication를 분리시키기 위해 주석처리
+//@EnableJpaAuditing // JPA Auditing 활성화
 @SpringBootApplication
 public class Application {
     public static void main(String args[]) {
---
+++ blog/src/main/java/net/blog/config/JpaConfig.java
@@ -0,0 +1,10 @@
+package net.blog.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@Configuration
+// JPA Auditiong 활성화
+@EnableJpaAuditing
+public class JpaConfig {
+}
