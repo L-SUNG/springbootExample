4038353511317dea8da8f51ab17b01bdfe39c38b
lsung <cp.lsung@gmail.com>
2023-01-24 04:13:37 +0900

    배포를 위한 war빌드 설정 - 유저 로그인시 유저 권한수정

--- blog/src/main/java/net/blog/Application.java
+++ blog/src/main/java/net/blog/Application.java
@@ -2,12 +2,21 @@ package net.blog;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 // EnableJpaAuditing와 SpringBootApplication를 분리시키기 위해 주석처리
 //@EnableJpaAuditing // JPA Auditing 활성화
 @SpringBootApplication
-public class Application {
+// 외장톰캣 사용시 SpringBootServletInitializer 상속필요
+public class Application extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(Application.class);
+    }
+
     public static void main(String args[]) {
         SpringApplication.run(Application.class, args);
     }
